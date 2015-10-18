0352e09929baf7b9ac307ddc25d8a6d7243bcdfb
Adams1986 <simonadams86@gmail.com>
2015-10-18 14:37:16 +0200

    Version with communication with server get-method to return an encrypted user and converting to a User-object

--- src/example/User.java
+++ src/example/User.java
@@ -1,23 +1,50 @@
 package example;
 
+import java.sql.Date;
+
+
 /**
- * Created by ADI on 16-10-2015.
+ * Created by Oscar on 12-10-2015.
  */
+//
 public class User {
 
+    //creating variables
     private int id;
-    private String username;
     private String firstName;
     private String lastName;
+    private String username;
     private String password;
+    private Date created;
+    private String status;
+    private String email;
+    private String type;
 
-    public User(int id, String username, String firstName, String lastName, String password) {
+
+    public User (int id, String firstName, String lastName, String email, String userName, String password, Date created, String status, String type ) {
         this.id = id;
-        this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
+        this.username = userName;
+        this.password = password;
+        this.created = created;
+        this.status = status;
+        this.type = type;
+    }
+
+    public User(int id, String firstName, String lastName, String username, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
         this.password = password;
     }
+
+    public User () {}
+
+    //creating get and set method for all the variables, so they can be used by other classes
+
 
     public int getId() {
         return id;
@@ -25,14 +52,6 @@ public class User {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getFirstName() {
@@ -51,6 +70,14 @@ public class User {
         this.lastName = lastName;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getPassword() {
         return password;
     }
@@ -59,9 +86,40 @@ public class User {
         this.password = password;
     }
 
+    public Date getCreated() {
+        return created;
+    }
 
-    @Override
-    public String toString() {
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    public String toJson() {
 
         String s = String.format("{ \"id\" : %d, \"username\" : \"%s\", \"firstName\" : \"%s\", " +
                 "\"lastName\" : \"%s\", \"password\" : \"%s\" }", id, username, firstName, lastName, password);
